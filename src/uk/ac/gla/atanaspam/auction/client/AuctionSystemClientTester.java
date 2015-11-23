7e3380f222e25f74ded7143bc51cce6714a56fba
Atanas Pamukchiev <atanas.pam@gmail.com>
2015-11-23 14:30:04 +0000

    Improved performance tester

--- src/uk/ac/gla/atanaspam/auction/client/AuctionSystemClientTester.java
+++ src/uk/ac/gla/atanaspam/auction/client/AuctionSystemClientTester.java
@@ -14,6 +14,7 @@ import java.util.Locale;
 public class AuctionSystemClientTester {
 
     public static void main (String[] args){
+
         AuctionSystemClientImpl client = null;
         try {
             client = new AuctionSystemClientImpl();
@@ -28,17 +29,101 @@ public class AuctionSystemClientTester {
         } catch (ParseException e) {
             e.printStackTrace();
         }
+
         try {
-            int n = 1;// client.createNewAuction(d, 100);
-            long startTime = System.currentTimeMillis();
-            for (int i = 202; i<10202; i++){
-                client.bid(n,i);
+            long startTime = 0;
+            long elapsedTime = 0;
+            switch (args[0]){
+                case "1":{
+                    startTime = System.currentTimeMillis();
+                    int n = 1;
+                    for (int i = 0; i<10000; i++){
+                        client.createNewAuction("auction "+i, d, 100);
+                    }
+                    break;
+                }
+                case "2":{
+                    startTime = System.currentTimeMillis();
+                    int n = client.createNewAuction("auction",d, 100);
+                    startTime = System.currentTimeMillis();
+                    for (int i = 0; i<10000; i++){
+                        client.bid(n,i);
+                    }
+                    break;
+                }
+                case "3":{
+                    startTime = System.currentTimeMillis();
+                    Thread t1 = testCreate(d, 5000);
+                    Thread t2 = testCreate(d, 5000);
+                    t1.start();
+                    t2.start();
+                    t2.join();
+                    break;
+                }
+                case "4":{
+                    startTime = System.currentTimeMillis();
+                    Thread t1 = testBid(d, 5000);
+                    Thread t2 = testBid(d, 5000);
+                    t1.start();
+                    t2.start();
+                    t2.join();
+                    break;
+                }
+
+                case "5":{
+                    startTime = System.currentTimeMillis();
+                    Thread t1 = testCreate(d, 5000);
+                    Thread t2 = testBid(d, 5000);
+                    t1.start();
+                    t2.start();
+                    t2.join();
+                    break;
+                }
+
             }
-            long elapsedTime = System.currentTimeMillis() - startTime;
+
+            elapsedTime = System.currentTimeMillis() - startTime;
             System.out.format("10000 calls in %d ms - %d.%03d ms/call\n",
                     elapsedTime, elapsedTime/10000, (elapsedTime%10000)/10);
+            System.exit(0);
         } catch (Exception e) {
             System.out.println("Cannot create acution...");
         }
+
+
+    }
+    public static Thread testCreate(Date d, int iterations) {
+        Thread t = new Thread(new Runnable() {
+            public void run() {
+                AuctionSystemClientImpl client;
+                try {
+                    client = new AuctionSystemClientImpl();
+                    for (int i = 0; i<iterations; i++){
+                        client.createNewAuction("auction "+i, d, 100);
+                    }
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        return t;
+    }
+
+    public static Thread testBid(Date d, int iterations) {
+        Thread t = new Thread(new Runnable() {
+            public void run() {
+                AuctionSystemClientImpl client;
+                try {
+                    client = new AuctionSystemClientImpl();
+                    int n = client.createNewAuction("auction",d, 100);
+                    for (int i = 0; i<iterations; i++){
+                        client.bid(n,i);
+                    }
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        return t;
     }
 }
